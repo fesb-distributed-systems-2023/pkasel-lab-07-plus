d47318a76bb8a708560cb70452e773c63131ef41
pkaselj <petar.kaselj.00@gmail.com>
2024-01-14 03:18:35 +0100

    Implemented gRPC API

--- gRPC-API/Program.cs
+++ gRPC-API/Program.cs
@@ -1,5 +1,7 @@
 using gRPC_API.Services;
-using REST_API.Logic.EmailLogic;
+using EasyMailCoreApplication.Logic;
+using EasyMailCoreApplication.Repositories;
+using EasyMailCoreApplication.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,13 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+
+builder.Services.Configure<ValidationConfiguration>(builder.Configuration.GetSection("Validation"));
+builder.Services.Configure<DBConfiguration>(builder.Configuration.GetSection("Database"));
+
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+builder.Services.AddSingleton<IEmailLogic, EmailLogic>();
 builder.Services.AddSingleton<IEmailRepository, EmailRepository_MSSQL>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<EmailService>();
+app.MapGrpcService<EmailAPI>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+IWebHostEnvironment env = app.Environment;
+
+if (env.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 
 app.Run();
--- gRPC-API/Services/EmailService.cs
+++ gRPC-API/Services/EmailService.cs
@@ -1,22 +1,61 @@
 using Grpc.Core;
 using gRPC_API;
 
+using EasyMailCoreApplication.Logic;
+using EasyMailCoreApplication.Models;
+
 namespace gRPC_API.Services
 {
-    public class EmailService : Greeter.GreeterBase
+    public class EmailAPI : EmailHandler.EmailHandlerBase
     {
-        private readonly ILogger<EmailService> _logger;
-        public EmailService(ILogger<EmailService> logger)
+        private readonly IEmailLogic _emailLogic;
+        public EmailAPI(IEmailLogic emailLogic)
         {
-            _logger = logger;
+            _emailLogic = emailLogic;
         }
 
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        public override Task<GetEmailsResponse> GetEmails(GetEmailsRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new HelloReply
+            var result = new GetEmailsResponse();
+            result.Emails.Clear();
+            result.Emails.Add(_emailLogic.GetEmails().Select( x => ModelToOutputType(x) ));
+            return Task.FromResult(result);
+        }
+
+        public override Task<CreateEmailResponse> CreateEmail(CreateEmailRequest request, ServerCallContext context)
+        {
+            _emailLogic.CreateNewEmail(InputTypeToModel(request.Email));
+            return Task.FromResult(new CreateEmailResponse { });
+        }
+
+        private EmailOutputType ModelToOutputType(Email model)
+        {
+            var output_type = new EmailOutputType
             {
-                Message = "Hello " + request.Name
-            });
+                Id = model.ID,
+                Subject = model.Subject,
+                Body = model.Body,
+                Timestamp = model.Timestamp.ToString(),
+                Sender = model.Sender
+            };
+
+            output_type.Receivers.Clear();
+            output_type.Receivers.AddRange(model.Receivers);
+
+            return output_type;
+        }
+
+        private Email InputTypeToModel(EmailInputType input_type)
+        {
+            return new Email
+            {
+                ID = -1,
+                Subject = input_type.Subject,
+                Body = input_type.Body,
+                Sender = input_type.Sender,
+                Timestamp = DateTime.MinValue,
+                Receivers = input_type.Receivers.Clone()
+            };
         }
     }
 }
